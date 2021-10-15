1674a5d912fd074772923800e2a479a3766ae32c
Emon Sheikh <sheikhemon@gmail.com>
2021-10-15 15:44:44 +0600

    practice tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-pink-900 text-white p-4">colors</p>
+    </div>
+  );
+}
+
+export default App;
